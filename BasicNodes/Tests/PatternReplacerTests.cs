63d06b75aa936c392cfbef3c84a26198aa704c9b
reven <john@reven.nz>
2021-11-26 13:32:30 +1300

    added pattern replacer

---
+++ BasicNodes/Tests/PatternReplacerTests.cs
@@ -0,0 +1,46 @@
+﻿#if(DEBUG)
+
+namespace BasicNodes.Tests
+{
+    using FileFlows.BasicNodes.Functions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PatternReplacerTests
+    {
+        [TestMethod]    
+        public void PatternReplacer_Basic()
+        {
+            PatternReplacer node = new PatternReplacer();
+            node.Replacements = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("Seinfeld", "Batman")
+            };
+            node.UnitTest = true;
+            var args = new FileFlows.Plugin.NodeParameters(@"c:\test\Seinfeld.mkv");
+
+            var result = node.Execute(args);
+            Assert.AreEqual(1, result);
+            Assert.AreEqual(@"c:\test\Batman.mkv", args.WorkingFile);
+        }
+
+        [TestMethod]
+        public void PatternReplacer_Regex()
+        {
+            PatternReplacer node = new PatternReplacer();
+            node.Replacements = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>(@"s([\d]+)e([\d]+)", "$1x$2"),
+                new KeyValuePair<string, string>(@"0([1-9]+x[\d]+)", "$1"),
+            };
+            node.UnitTest = true;
+            var args = new FileFlows.Plugin.NodeParameters(@"c:\test\Seinfeld S03E06.mkv");
+
+            var result = node.Execute(args);
+            Assert.AreEqual(1, result);
+            Assert.AreEqual(@"c:\test\Seinfeld 3x06.mkv", args.WorkingFile);
+        }
+    }
+}
+
+#endif
