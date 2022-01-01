e5f843c8718e82e3138e19d2b82b10e9e02ed7d7
reven <john@reven.nz>
2022-01-01 14:48:16 +1300

    added goto flow node

---
+++ BasicNodes/Functions/GotoFlow.cs
@@ -0,0 +1,24 @@
+﻿namespace FileFlows.BasicNodes.Functions
+{
+    using FileFlows.Plugin;
+    using FileFlows.Plugin.Attributes;
+
+    public class GotoFlow : Node
+    {
+        public override int Inputs => 1;
+        public override int Outputs => 0;
+
+        public override FlowElementType Type => FlowElementType.Logic;
+        public override string Icon => "fas fa-sitemap";
+
+        [Select("FLOW_LIST", 1)]
+        public ObjectReference Flow { get; set; }
+
+
+        public override int Execute(NodeParameters args)
+        {
+            args.GotoFlow(Flow);
+            return 0;
+        }
+    }
+}
---
+++ BasicNodes/Tests/ExecutorTests.cs
@@ -0,0 +1,30 @@
+﻿#if(DEBUG)
+
+namespace BasicNodes.Tests
+{
+    using FileFlows.BasicNodes.File;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ExecutorTests
+    {
+        [TestMethod]
+        public void Executor_OutputVariable()
+        {
+            var logger = new TestLogger();
+            var args = new FileFlows.Plugin.NodeParameters(@"c:\test\testfile.mkv", logger, false, string.Empty);
+
+            Executor node = new Executor();
+            string file = @"D:\Videos\dummy.mkv";
+            node.FileName = @"C:\utils\ffmpeg\ffmpeg.exe";
+            node.Arguments = "-i \"" + file + "\"";
+            node.OutputVariable = "ExecOutput";
+            var result = node.Execute(args);
+            Assert.IsTrue(args.Variables.ContainsKey("ExecOutput"));
+            string output = args.Variables["ExecOutput"] as string;
+            Assert.IsNotNull(output);
+        }
+    }
+}
+
+#endif
