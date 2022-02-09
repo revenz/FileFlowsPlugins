701d19cd45d5c6e91299f78c96c1abcb099f3339
John Andrews <john@reven.nz>
2022-02-10 08:20:14 +1300

    added original file node

---
+++ BasicNodes/File/OriginalFile.cs
@@ -0,0 +1,21 @@
+﻿namespace FileFlows.BasicNodes.File
+{
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using FileFlows.Plugin;
+
+    public class OriginalFile : Node
+    {
+        public override int Inputs => 1;
+        public override int Outputs => 1;
+        public override string Icon => "fas fa-file";
+        public override FlowElementType Type => FlowElementType.Logic;
+
+        public override int Execute(NodeParameters args)
+        {
+            args.SetWorkingFile(args.FileName);
+            args.Logger?.ILog("Set working file to: " + args.FileName);
+            return 1;
+        }
+    }
+}
