bac17d9a9b03f01ebde2e4c455ec36dc05d995c8
John Andrews <john@reven.nz>
2022-04-22 16:29:36 +1200

    added Touch node

---
+++ BasicNodes/File/Touch.cs
@@ -0,0 +1,59 @@
+namespace FileFlows.BasicNodes.File;
+
+using FileFlows.Plugin;
+using FileFlows.Plugin.Attributes;
+
+public class Touch : Node
+{
+    public override int Inputs => 1;
+    public override int Outputs => 1;
+    public override FlowElementType Type => FlowElementType.Process;
+    public override string Icon => "fas fa-hand-point-right";
+
+    [TextVariable(1)]
+    public string FileName { get; set; }
+
+    public override int Execute(NodeParameters args)
+    {
+        string filename = args.ReplaceVariables(this.FileName ?? string.Empty, stripMissing: true);
+        if (string.IsNullOrEmpty(filename))
+            filename = args.WorkingFile;
+
+        if (IsDirectory(filename))
+        {
+            try
+            {
+                var dir = new DirectoryInfo(filename);
+                Directory.SetLastWriteTimeUtc(filename, DateTime.UtcNow);
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                args.Logger?.ELog("Failed to touch directory: " + ex.Message);
+                return -1;
+            }
+        }
+        else
+        {
+            try
+            {
+                System.IO.File.SetLastWriteTimeUtc(filename, DateTime.UtcNow);
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                args.Logger?.ELog($"Failed to touch file: '{filename}' => {ex.Message}");
+                return -1;
+            }
+        }
+    }
+
+    private bool IsDirectory(string filename)
+    {
+        try
+        {
+            return new DirectoryInfo(filename).Exists;
+        }
+        catch (Exception) { return false; }
+    }
+}
---
+++ BasicNodes/Tests/TouchTests.cs
@@ -0,0 +1,38 @@
+﻿#if(DEBUG)
+
+namespace BasicNodes.Tests;
+
+using FileFlows.BasicNodes.File;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class TouchTests
+{
+    FileFlows.Plugin.NodeParameters Args;
+
+    [TestInitialize]
+    public void TestStarting()
+    {
+        Args = new FileFlows.Plugin.NodeParameters(@"c:\test\testfile.mkv", new TestLogger(), false, string.Empty);
+    }
+
+    [TestMethod]
+    public void Touch_File()
+    {
+        Touch node = new ();
+        node.FileName = @"D:\videos\testfiles\basic.mkv";
+        var result = node.Execute(Args);
+        Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void Touch_Folder()
+    {
+        Touch node = new();
+        node.FileName = @"D:\videos\testfiles";
+        var result = node.Execute(Args);
+        Assert.AreEqual(1, result);
+    }
+}
+
+#endif
