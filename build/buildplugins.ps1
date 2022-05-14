defabcd73bfbb5a284ae1941e9fc21e4434649fa
John Andrews <john@reven.nz>
2022-05-14 14:10:45 +1200

    increased plugin version umber

--- build/buildplugins.ps1
+++ build/buildplugins.ps1
@@ -23,7 +23,7 @@ $output = $output | Resolve-Path
 Remove-Item Builds  -Recurse -ErrorAction SilentlyContinue
 
 $revision = (git rev-list --count --first-parent HEAD) -join "`n"
-$version = "0.5.4.$revision"
+$version = "0.6.0.$revision"
 
 $json = "[`n"
 
