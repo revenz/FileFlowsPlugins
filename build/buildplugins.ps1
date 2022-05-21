69eb37139907b791610cb3246b37fd4d0a153106
John Andrews <john@reven.nz>
2022-05-22 10:59:01 +1200

    increased version number

--- build/buildplugins.ps1
+++ build/buildplugins.ps1
@@ -23,7 +23,7 @@ $output = $output | Resolve-Path
 Remove-Item Builds  -Recurse -ErrorAction SilentlyContinue
 
 $revision = (git rev-list --count --first-parent HEAD) -join "`n"
-$version = "0.6.2.$revision"
+$version = "0.6.3.$revision"
 
 $json = "[`n"
 
