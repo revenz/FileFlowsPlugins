b8ef39ac0749ae44fadb8a126223d878b747ca2a
John Andrews <john@reven.nz>
2022-06-13 15:15:29 +1200

    renamed ffmpeg builder set audio language to set language and now does subtitles as well as audio

--- VideoNodes/FfmpegBuilderNodes/Audio/FfmpegBuilderAudioSetLanguage.cs
+++ VideoNodes/FfmpegBuilderNodes/Audio/FfmpegBuilderAudioSetLanguage.cs
@@ -4,26 +4,65 @@ namespace FileFlows.VideoNodes.FfmpegBuilderNodes;
 
 public class FfmpegBuilderAudioSetLanguage : FfmpegBuilderNode
 {
-    public override string HelpUrl => "https://docs.fileflows.com/plugins/video-nodes/ffmpeg-builder/audio-set-language";
+    public override string HelpUrl => "https://docs.fileflows.com/plugins/video-nodes/ffmpeg-builder/set-language";
 
     public override int Outputs => 2;
 
     public override string Icon => "fas fa-comment-dots";
 
+
+    [Select(nameof(StreamTypeOptions), 1)]
+    public string StreamType { get; set; }
+
     [Required]
-    [Text(1)]
+    [Text(2)]
     public string Language { get; set; }
+
+    private static List<ListOption> _StreamTypeOptions;
+    public static List<ListOption> StreamTypeOptions
+    {
+        get
+        {
+            if (_StreamTypeOptions == null)
+            {
+                _StreamTypeOptions = new List<ListOption>
+                {
+                    new ListOption { Label = "Audio", Value = "Audio" },
+                    new ListOption { Label = "Subtitle", Value = "Subtitle" },
+                    new ListOption { Label = "Both", Value = "Both" },
+                };
+            }
+            return _StreamTypeOptions;
+        }
+    }
 
     public override int Execute(NodeParameters args)
     {
         bool changes = false;
-        foreach (var at in Model.AudioStreams)
+        if (StreamType == "Subtitle" || StreamType == "Both")
         {
-            if (string.IsNullOrEmpty(at.Language))
+
+            foreach (var at in Model.AudioStreams)
             {
-                at.Language = Language.ToLower();
-                at.ForcedChange = true; // this will ensure the language is set even if there are no changes anywhere else
-                changes = true;
+                if (string.IsNullOrEmpty(at.Language))
+                {
+                    at.Language = Language.ToLower();
+                    at.ForcedChange = true; // this will ensure the language is set even if there are no changes anywhere else
+                    changes = true;
+                }
+            }
+        }
+        
+        if(string.IsNullOrEmpty(StreamType) || StreamType == "Both" || StreamType == "Audio")
+        {
+            foreach (var at in Model.AudioStreams)
+            {
+                if (string.IsNullOrEmpty(at.Language))
+                {
+                    at.Language = Language.ToLower();
+                    at.ForcedChange = true; // this will ensure the language is set even if there are no changes anywhere else
+                    changes = true;
+                }
             }
         }
         return changes ? 1 : 2;
