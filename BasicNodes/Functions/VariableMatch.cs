76b8ecdd91c399c014fe07ce18b92817a80998a5
John Andrews <john@reven.nz>
2022-09-25 21:54:55 +1300

    FF-333 - added variable matches

---
+++ BasicNodes/Functions/VariableMatch.cs
@@ -0,0 +1,39 @@
+﻿using FileFlows.Plugin;
+using FileFlows.Plugin.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicNodes.Functions;
+
+/// <summary>
+/// Tests if an input value is matched by a variable
+/// </summary>
+public class VariableMatch : Node
+{
+    public override int Inputs => 1;
+    public override int Outputs => 2;
+
+    public override FlowElementType Type => FlowElementType.Logic;
+    public override string HelpUrl => "https://docs.fileflows.com/plugins/basic-nodes/variable-match";
+    public override string Icon => "fas fa-equals";
+
+    [Required]
+    [Select("VARIABLE_LIST", 1)]
+    public ObjectReference Variable { get; set; }
+
+    [Required]
+    [TextVariable(2)]
+    public string Input { get; set; }
+
+
+
+    public override int Execute(NodeParameters args)
+    {
+        bool matches = args.MatchesVariable(Variable?.Name, Input);
+        return matches ? 1 : 2;
+    }
+}
