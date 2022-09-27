647c055925c2795010a1414e739bf76e2f2ea62b
John Andrews <john@reven.nz>
2022-09-28 11:48:50 +1300

    av1 docker

--- VideoNodes/FfmpegBuilderNodes/FfmpegBuilderExecutor.cs
+++ VideoNodes/FfmpegBuilderNodes/FfmpegBuilderExecutor.cs
@@ -132,24 +132,20 @@ namespace FileFlows.VideoNodes.FfmpegBuilderNodes
             if ((strFfArgs.Contains("libaom-av1") || strFfArgs.Contains("libsvtav1")))
             {
                 args.Logger.DLog("Using AV1");
-                if (args.IsDocker)
+                if (File.Exists(ffmpeg + "-av1"))
                 {
-                    args.Logger.DLog("Using AV1 on docker");
-                    if (File.Exists(ffmpeg + "-av1"))
+                    ffmpeg = ffmpeg + "-av1";
+                    if(ffArgs.IndexOf("-hwaccel") > 0)
                     {
-                        ffmpeg = ffmpeg + "-av1";
-                        if(ffArgs.IndexOf("-hwaccel") > 0)
+                        ffArgs.RemoveRange(ffArgs.IndexOf("-hwaccel"), 2);
+                        if(ffArgs.IndexOf("-hwaccel_output_format") > 0)
                         {
-                            ffArgs.RemoveRange(ffArgs.IndexOf("-hwaccel"), 2);
-                            if(ffArgs.IndexOf("-hwaccel_output_format") > 0)
-                            {
-                                ffArgs.RemoveRange(ffArgs.IndexOf("-hwaccel_output_format"), 2);
-                            }
+                            ffArgs.RemoveRange(ffArgs.IndexOf("-hwaccel_output_format"), 2);
                         }
                     }
-                    else
-                        args.Logger.DLog("Did not find custom FFMPEG AV1: " + ffmpeg + "-av1");
-                }                
+                }
+                else
+                    args.Logger.DLog("Did not find custom FFMPEG AV1: " + ffmpeg + "-av1");
             }
 
 
