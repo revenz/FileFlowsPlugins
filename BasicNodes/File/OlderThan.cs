1a102c8ab0536843dc3e37d0deab6f67afafa4a3
john <john@reven.nz>
2022-11-17 16:32:30 +1300

    FF-364 - added unzip node

--- BasicNodes/File/OlderThan.cs
+++ BasicNodes/File/OlderThan.cs
@@ -15,40 +15,12 @@ public class OlderThan : Node
     public override string Icon => "fas fa-clock";
     public override string HelpUrl => "https://docs.fileflows.com/plugins/basic-nodes/older-than";
 
-
-    [NumberInt(1)]
-    public int Number { get; set; }
-
-    [NumberInt(2)]
-    [Select(nameof(UnitOptions), 1)]
-    public int Unit { get; set; }
+    [Period(1)]
+    public int Minutes { get; set; }
     
-    [NumberInt(3)]
+    [NumberInt(2)]
     [Select(nameof(DateOptions), 1)]
     public int Date { get; set; }
-    
-    
-    private static List<ListOption> _UnitOptions;
-    /// <summary>
-    /// Gets or sets the unit options
-    /// </summary>
-    public static List<ListOption> UnitOptions
-    {
-        get
-        {
-            if (_UnitOptions == null)
-            {
-                _UnitOptions = new List<ListOption>
-                {
-                    new () { Label = "Minutes", Value = 1 },
-                    new () { Label = "Days", Value = 1440 },
-                    new () { Label = "Weeks", Value = 10080 }
-                };
-            }
-            return _UnitOptions;
-        }
-    }
-    
     
     private static List<ListOption> _DateOptions;
     /// <summary>
@@ -82,11 +54,7 @@ public class OlderThan : Node
 
         var date = Date == 1 ? fi.LastWriteTime : fi.CreationTime;
 
-        // now, 1 week ago, == 7 days
         var minutes = DateTime.Now.Subtract(date).TotalMinutes;
-        // 3 * 1 days, == 3 days
-        var cutoff = this.Number * Math.Max(this.Unit, 1);
-        // 7 > 3 == true, 1
-        return minutes > cutoff ? 1 : 2;
+        return minutes > this.Minutes ? 1 : 2;
     }
 }
