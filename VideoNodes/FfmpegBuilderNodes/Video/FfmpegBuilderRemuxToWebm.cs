d27c7dce34afe3cc67fada5d17ef1773b454c3c3
john <john@reven.nz>
2023-02-05 13:40:16 +1300

    FF-394 - added remux to webm

---
+++ VideoNodes/FfmpegBuilderNodes/Video/FfmpegBuilderRemuxToWebm.cs
@@ -0,0 +1,12 @@
+﻿namespace FileFlows.VideoNodes.FfmpegBuilderNodes;
+
+public class FfmpegBuilderRemuxToWebm : FfmpegBuilderNode
+{
+    public override string HelpUrl => "https://docs.fileflows.com/plugins/video-nodes/ffmpeg-builder/remux-to-webm";
+
+    public override int Execute(NodeParameters args)
+    {
+        this.Model.Extension = "webm";
+        return 1;
+    }
+}
