05397999b711b076bfb66f79067c79a477924df5
john <john@reven.nz>
2023-02-11 17:09:32 +1300

    FF-413 - added static functions to Video nodes

---
+++ VideoNodes/StaticMethods.cs
@@ -0,0 +1,16 @@
+namespace FileFlows.VideoNodes;
+
+/// <summary>
+/// Special class that exposes static methods to the script executor
+/// </summary>
+public class StaticMethods
+{
+    /// <summary>
+    /// Gets the video info for a file
+    /// </summary>
+    /// <param name="args">the args</param>
+    /// <param name="filename">the name of the file to read</param>
+    /// <returns>the video info</returns>
+    public static VideoInfo GetVideoInfo(NodeParameters args, string filename)
+        => VideoInfoHelper.ReadStatic(args.Logger, args.GetToolPath("FFMpeg"), filename);
+}
